acb839da0d0ba9b3dd8da56ba7f825330d663215
smessie <smessie@smessie.com>
2023-01-11 23:01:12 +0100

    refactor: Remove proof by directly passing `--nope` to swipl query

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { SwiplEye, queryOnce } from 'eyereasoner';
-import {DataFactory, Parser, Store, Writer} from "n3";
 
 export async function n3reasoner(data, query) {
     // Keep results in an array
@@ -13,44 +12,10 @@ export async function n3reasoner(data, query) {
     Module.FS.writeFile('query.n3', query);
 
     // Execute main(['--quiet', './data.n3', '--query', './query.n3']).
-    queryOnce(Module, 'main', ['--quiet', './data.n3', '--query', './query.n3']);
+    queryOnce(Module, 'main', ['--nope', '--quiet', './data.n3', '--query', './query.n3']);
     console.log('result', queryResults.join('\n'));
 
-    // Parse result string into proof and resulting triples
-    const parser = new Parser({ format: 'text/n3' });
-    const proof = parser.parse(queryResults.join('\n'));
-    const store = new Store(proof);
-
-    const proofNode = store.getSubjects(
-        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-        'http://www.w3.org/2000/10/swap/reason#Proof',
-        DataFactory.defaultGraph(),
-    );
-
-    if (proofNode.length !== 1) {
-        throw new Error(`Expected exactly one proof: received ${proofNode.length}`);
-    }
-
-    const results = store.getObjects(
-        proofNode[0],
-        'http://www.w3.org/2000/10/swap/reason#gives',
-        DataFactory.defaultGraph(),
-    );
-
-    if (results.length !== 1) {
-        throw new Error(`Expected exactly one triple giving inference results from proof: received ${results.length}`);
-    }
-
-    const result = store.getQuads(null, null, null, results[0])
-        .map((res) => DataFactory.quad(res.subject, res.predicate, res.object));
-
-    // Convert Quads to Turtle strings
-    const writer = new Writer({});
-    writer.addQuads(result);
-    const turtle = [];
-    writer.end((error, result) => turtle.push(result));
-
-    return turtle.join('\n');
+    return queryResults.join('\n');
 }
 
 export default {
