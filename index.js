f7222ff389d14636810d8c4da2673f82cdf3ced7
smessie <smessie@smessie.com>
2023-01-27 10:55:38 +0100

    feat: Add support for all pass output types via new option

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ import { SwiplEye, queryOnce } from 'eyereasoner';
 export async function n3reasoner(data, query, options = {}) {
     // Check options
     const unknownOptions = Object.keys(options).filter(
-        (key) => !["blogic"].includes(key)
+        (key) => !["output", "blogic"].includes(key)
     );
     if (unknownOptions.length > 0) {
         throw new Error(
             "Unknown options: " + unknownOptions.join(", ")
         );
     }
-    const { blogic = false } = options;
+    const { output = "derivations", blogic = false } = options;
+
+    const pass_map = {
+        'derivations': '--pass-only-new',
+        'deductive_closure': '--pass',
+        'deductive_closure_plus_rules': '--pass-all',
+        'grounded_deductive_closure_plus_rules': '--pass-all-ground',
+    }
+    // Check if output is valid
+    if (!pass_map.hasOwnProperty(output)) {
+        throw new Error("Unknown output option: " + output);
+    }
+    const pass = pass_map[output];
 
     // Keep results in an array
     const queryResults = [];
@@ -25,7 +37,7 @@ export async function n3reasoner(data, query, options = {}) {
     }
 
     // Execute main(['--quiet', './data.n3', '--query', './query.n3']).
-    queryOnce(Module, 'main', [blogic ? '--blogic' : '--nope', '--quiet', './data.n3', ...(blogic ? [] : ['--query', './query.n3'])]);
+    queryOnce(Module, 'main', [blogic ? '--blogic' : '--nope', '--quiet', pass, './data.n3', ...(blogic ? [] : ['--query', './query.n3'])]);
     console.log('result', queryResults.join('\n'));
 
     return queryResults.join('\n');
